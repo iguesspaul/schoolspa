b874df4e58ae4e3ec728221411e50a3fa8a70e4c
iguesspaul <paul.l.vantilburg@gmail.com>
2022-10-19 08:18:27 -0400

    very coool committime

--- src/routes/todo/CreateTask.svelte
+++ src/routes/todo/CreateTask.svelte
@@ -1,7 +1,12 @@
 <script>
-    import Plus from 'iconoir/icons/plus.svg';
+	import Plus from 'iconoir/icons/plus.svg';
 </script>
 
-<div class="w-14 h-14 hover:w-16 hover:h-16 duration-300 ease-in-out delay-75 rounded-full absolute bottom-0 right-0 mb-2 mr-2 bg-gray-500 hover:bg-gray-700 flex justify-center items-center">
-    <img class="w-3/4 h-3/4" src={Plus} alt="plus" />
-</div>
+<button
+	class="group overflow-hidden w-14 h-14 hover:w-16 hover:h-16 duration-300 ease-in-out delay-75 rounded-full absolute bottom-0 right-0 mb-2 mr-2 bg-gray-500 flex justify-center items-center"
+>
+	<div class="w-[180%] h-full absolute left-0 top-0 flex items-center justify-center">
+		<div class="w-full aspect-square origin-left transition -translate-x-full bg-gray-600 group-enabled:group-hover:translate-x-[-40%]" />
+	</div>
+	<img class="w-3/4 h-3/4 z-10" src={Plus} alt="plus" />
+</button>
