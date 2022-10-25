d6abbf891b04d061a2bbb14501c785b8fc7dbf86
iguesspaul <paul.l.vantilburg@gmail.com>
2022-10-25 19:35:35 +0200

    fixed opacity on overlay

--- src/components/Overlay.svelte
+++ src/components/Overlay.svelte
@@ -5,7 +5,7 @@
 
 <div class="fixed top-0 left-0 flex items-center justify-center w-screen h-screen">
 	<div
-		class="relative flex items-center justify-center w-1/5 rounded-lg h-1/5 bg-slate-600/[.6] shadow-clay"
+		class="relative flex items-center justify-center w-1/5 rounded-lg h-1/5 bg-slate-600 shadow-clay"
 	>
 		<button
 			class="absolute top-0 right-0 m-2 bg-red-700 rounded-full "
